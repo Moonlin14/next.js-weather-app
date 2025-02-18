9994c8ea85715e8f69b9087ced63e7f19bcf1d7c
Danil Sidelnikov <danilsidelnikov74@mail.ru>
2025-02-18 14:48:54 +0300

    add weather api

---
+++ app/action.ts
@@ -0,0 +1,11 @@
+'use server';
+
+export const getWheatherData = async (city: string) => {
+  try {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.OPENWEATHERMAP_API_KEY}`);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
